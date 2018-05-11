a940fb13f359cc4ba04eb98a6919f30bf0b5d701
Christian Svensson <blue@cmd.nu>
2018-05-11 09:37:23 +0200

    use secure grpc

--- prodaccess.go
+++ prodaccess.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"crypto/tls"
 	"flag"
 	"io/ioutil"
 	"log"
@@ -11,19 +12,20 @@ import (
 	"github.com/google/uuid"
 	"golang.org/x/net/context"
 	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials"
 	pb "github.com/dhtech/proto/auth"
 )
 
 var (
-	address   = flag.String("address", "auth.tech.dreamhack.se:1214", "Authentication server to use.")
-	cors      = flag.String("cors", "https://auth.tech.dreamhack.se", "Domain to reply to ident requests from.")
-	ident     = ""
-	sshpubkey = flag.String("sshpubkey", "$HOME/.ssh/id_ecdsa.pub", "SSH public key to request signed.")
+	grpcAddress  = flag.String("grpc", "auth-grpc.tech.dreamhack.se:443", "Authentication server to use.")
+	webUrl       = flag.String("web", "https://auth.tech.dreamhack.se", "Domain to reply to ident requests from")
+	ident        = ""
+	sshPubKey    = flag.String("sshpubkey", "$HOME/.ssh/id_ecdsa.pub", "SSH public key to request signed.")
 )
 
 func presentIdent(w http.ResponseWriter, r *http.Request) {
 	w.Header().Add("Content-Type", "text/plain")
-	w.Header().Add("Access-Control-Allow-Origin", *cors)
+	w.Header().Add("Access-Control-Allow-Origin", *webUrl)
 	w.Write([]byte(ident))
 }
 
@@ -34,14 +36,16 @@ func main() {
 	go http.ListenAndServe(":1215", nil)
 
 	// Set up a connection to the server.
-	conn, err := grpc.Dial(*address, grpc.WithInsecure())
+	conn, err := grpc.Dial(*grpcAddress, grpc.WithTransportCredentials(
+		credentials.NewTLS(&tls.Config{}),
+	))
 	if err != nil {
 		log.Fatalf("did not connect: %v", err)
 	}
 	defer conn.Close()
 	c := pb.NewAuthenticationServiceClient(conn)
 	
-	key, err := ioutil.ReadFile(os.ExpandEnv(*sshpubkey))
+	key, err := ioutil.ReadFile(os.ExpandEnv(*sshPubKey))
 	if err != nil {
 		log.Fatalf("could not read SSH public key: %v", err)
 	}
@@ -70,7 +74,7 @@ func main() {
 		}
 		log.Printf("Response: %v", response)
 		if (response.RequiredAction != nil) {
-			openUrl("http://auth.tech.dreamhack.se" + response.RequiredAction.Url)
+			openUrl(*webUrl + response.RequiredAction.Url)
 		} else {
 			break
 		}
