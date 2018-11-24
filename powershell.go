0d800b4641b7486396f3c28a39e4f16d87d3d3b6
Christian Svensson <blue@cmd.nu>
2018-11-24 11:57:23 +0100

    Add support for importing certs under WSL

---
+++ powershell.go
@@ -0,0 +1,11 @@
+package main
+
+const (
+	psPurge = `
+Get-ChildItem -Path cert:\CurrentUser\My -Recurse -EKU "*Client Authentication*" -ExpiringInDays 0 |
+  Remove-Item
+`
+	psImport = `
+Import-PfxCertificate -FilePath %s -CertStoreLocation Cert:\CurrentUser\My
+`
+)
