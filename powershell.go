8d7aea19621ab655baf995cf88ba922f62a33245
Christian Svensson <blue@cmd.nu>
2018-11-25 09:19:08 +0100

    Remove newline in Powershell

--- powershell.go
+++ powershell.go
@@ -2,8 +2,7 @@ package main
 
 const (
 	psPurge = `
-Get-ChildItem -Path cert:\CurrentUser\My -Recurse -EKU "*Client Authentication*" -ExpiringInDays 0 |
-  Remove-Item
+Get-ChildItem -Path cert:\CurrentUser\My -Recurse -EKU "*Client Authentication*" -ExpiringInDays 0 | Remove-Item
 `
 	psImport = `
 Import-PfxCertificate -FilePath %s -CertStoreLocation Cert:\CurrentUser\My
